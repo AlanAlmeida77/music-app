231291a51d1a2e6c9389190afaf70a955f169281
Alan Almeida <alandavidalmeida@gmail.com>
2023-08-06 18:22:13 -0300

    feature #Library_2

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,12 +1,34 @@
 "use client";
+import {TbPlaylist} from "react-icons/tb";
 
 const Library = () => {
     const onClick = () => {
         // handle upload 
     };
     return (
-        <div>
-            Library
+        <div className="flex flex-col">
+    <div
+        className="
+        flex
+        items-center
+        justify-betweeen
+        px-5
+        pt-4
+        "
+    >
+        <div 
+        className="
+        inline-flex
+        items-center
+        gap-x-2
+        "
+        >
+            <TbPlaylist />
+            </div>
+
         </div>
+    </div>
     )
 }
+
+export default Library;
