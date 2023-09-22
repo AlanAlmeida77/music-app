414d32835067afc09df00a0cba457f3de2b52af1
Alan Almeida <alandavidalmeida@gmail.com>
2023-09-22 16:21:35 -0300

    mod styles of layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
@@ -13,9 +14,32 @@ export default function Home() {
     <Header>
     <div
     className="mb-2">
-    <h1>
+    <h1
+      className="
+      text-white
+      text-3x1
+      font-semibold
+
+      "
+    >
       Welcome back
     </h1>
+    <div
+      className="
+      grid
+      grid-cols-1
+      sm:grid-cols-2
+      xl:grid-cols-3
+      gap-3
+      mt-4
+      "
+    >
+      <ListItem
+        image="/public/images/liked.png"
+        name="Liked Songs"
+        href="liked"
+      />
+    </div>
 
     </div>
     </Header>
